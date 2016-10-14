1818b73836546740758d1cc9f0854a50432b038a
ibowen <berwin.lew@gmail.com>
2016-10-14 11:32:23 -0700

    feat: add routing configuration

---
+++ venv/quotes.py
@@ -0,0 +1,8 @@
+def funny_quotes():
+	return [{
+		"author": "Chelsea Anderson",
+		"quote": "The more you weigh the harder you are to kidnap. Stay safe. Eat cake"
+	}, {
+		"author": "Billi Sims",
+		"quote": "Pin for Later: Funny Quotes About Working Out (or Rather, NOT Working Out)"
+	}]
